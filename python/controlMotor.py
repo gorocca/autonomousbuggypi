4d42ed8732d87256c573b58204d47f8cefff8020
gorocca <gonzy1235@hotmail.com>
2016-10-25 19:11:21 +0200

    Cambios en el control motor

--- python/controlMotor.py
+++ python/controlMotor.py
@@ -1,18 +1,25 @@
 import time
 import RPi.GPIO as GPIO
 
+class controlMotor(object):
 
-def calibraMotor():
-        print("Calibrando motor...")
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(7, GPIO.OUT)
-        t1 = GPIO.PWM(7, 50)
-        t1.start(0)
-        t1.ChangeDutyCycle(7.5)
-        time.sleep(3)
-        t1.ChangeDutyCycle(9.5)
-        time.sleep(3)
-        t1.ChangeDutyCycle(5.5)
-        time.sleep(3)
-        print("Calibrado motor terminado")
+        def __init__(self, t1):
+                GPIO.setwarnings(False)
+                GPIO.setmode(GPIO.BOARD)
+                GPIO.setup(7, GPIO.OUT)
+                self.t1 = GPIO.PWM(7, 50)
+                calibraMotor()
+
+        def calibraMotor():
+                print("Calibrando motor...")
+                t1.start(0)
+                t1.ChangeDutyCycle(7.5)
+                time.sleep(3)
+                t1.ChangeDutyCycle(9.5)
+                time.sleep(3)
+                t1.ChangeDutyCycle(5.5)
+                time.sleep(3)
+                print("Calibrado motor terminado")
+
+        def cambiaVelocidadMotor(float velocidad):
+                pass        
